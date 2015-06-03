0dc2cc997f0bbf982a66c767ccda179a6aebbd2d
Paul Johnson <paul@johnson.kiwi.nz>
2015-06-03 21:47:30 +1200

    start of test file, package.json, script and .gitignore

---
+++ test/test.js
@@ -0,0 +1,41 @@
+/*
+ * test.js
+ * Copyright (C) 2015 pauljohnson <pauljohnson@Paul-Johnsons-MacBook-Pro.local>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+var expect = require('chai').expect;
+
+describe('Versioned file structure', function() {
+    before(function() {
+    });
+
+    beforeEach(function() {
+    });
+
+    afterEach(function() {
+    });
+
+    after(function() {
+    });
+
+    describe('Add commit', function() {
+        it('should add commit by branch', function() {
+        });
+        it('should add commit by HEAD reference', function() {
+        });
+        it('should add commit by id', function() {
+        });
+    });
+    describe('Delete commit', function() {
+        it('should delete a commit by id', function() {
+        });
+    });
+    describe('List commits', function() {
+        it('should list all installed commits', function() {
+        });
+    });
+});
+
+

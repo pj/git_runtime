ecd75013e5bf4ace02a6185618244ae7e7a821a9
Paul Johnson <paul@johnson.kiwi.nz>
2016-08-24 13:34:11 -0400

    WIP on typescript conversion

--- src/lazycloudd.js
+++ src/lazycloudd.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node --harmony
-
+/// <reference path="../typings/index.d.ts" />
 /**
   * @file Script to start the lazy cloud proxy process that receives requests and routes
   * them to the correct commit id process.

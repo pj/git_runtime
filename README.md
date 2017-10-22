49e9bf9731142df3f63beb658d53ec261fa46a85
Paul Johnson <paul@johnson.kiwi.nz>
2017-10-21 17:00:21 -0700

    Move postgres test files, minor cleanup and update TODO

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # git_runtime
 WIP
+
+# TODO:
+
+## Proxy
+
+- Fix tests
+- Code formatting
+
+## Database
+
+- Support primary keys including composite keys
+- Fix deleting + better tests.
+- Snapshot support.
+
+## Features
--- test/database/postgres/runpg.sh
+++ test/database/postgres/runpg.sh
@@ -3,4 +3,4 @@
 psql -q -d test -f test/database/postgres/helpers.pgsql
 psql -q -d test -f database/sql/postgres.pgsql
 psql -q -d test -f test/database/postgres/test_setup.pgsql 1>/dev/null
-/usr/local/Cellar/perl/5.26.0/bin/pg_prove -d test --ext .pgsql test/database/postgres
+/usr/local/Cellar/perl/5.26.0/bin/pg_prove -d test --ext .pgsql test/database/postgres/tests
